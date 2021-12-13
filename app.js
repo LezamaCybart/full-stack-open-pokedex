6b77bc28043caff387d0dc4d7f5afbb790be5211
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-12-13 16:11:29 -0300

    fixing health check

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
-
 app.get('/health', (req, res) => {
   res.send('ok')
 })
@@ -11,6 +8,9 @@ app.get('/health', (req, res) => {
 app.get('/version', (req, res) => {
   res.send('1')
 })
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
